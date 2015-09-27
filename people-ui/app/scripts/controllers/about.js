3fe41cc8042658a3e63ae50864cd4f50dd88b3dc
Clifton McIntosh <cliff.mcintosh@gmail.com>
2015-09-27 12:26:00 -0500

    generated basic AngularJS app with yeoman angular generator (https://github.com/yeoman/generator-angular).

---
+++ people-ui/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name peopleUiApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the peopleUiApp
+ */
+angular.module('peopleUiApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ people-ui/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name peopleUiApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the peopleUiApp
+ */
+angular.module('peopleUiApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
